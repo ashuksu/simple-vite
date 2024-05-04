64c3bc75039c831b25dadff6afa57e9b21c2f825
ashuksu <ashuksu@gmail.com>
2024-05-04 19:07:01 +0200

    dev - add Header and Footer components

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className='header shrink-0 bg-gray-800'>
+            <div className="container flex justify-center items-center py-3  text-white">
+                Header
+            </div>
+        </header>
+    )
+}
