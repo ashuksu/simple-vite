99fa2f2941d6852dcc5703700627eb4b130c8702
ashuksu <ashuksu@gmail.com>
2024-05-05 09:42:46 +0200

    dev - update Title component imports and add margin to improve layout in Main and SidebarMenu

--- src/components/layouts/SidebarMenu.tsx
+++ src/components/layouts/SidebarMenu.tsx
@@ -1,10 +1,11 @@
-import Title from "../ui/Title.tsx";
-import TrackDetails from "../TrackDetails.tsx";
+import Title from "../ui/Title";
+import TrackDetails from "../TrackDetails";
+import {Game} from "../../game/Game";
 
 export default function SidebarMenu({trackId}: {trackId: string | null}) {
     return (
         <aside className="sidebar-menu w-1/2">
-            <Title title='Track Details' styleCSS='text-gray-50'/>
+            <Title title='Track Details' styleCSS='text-gray-50 mb-5'/>
             <TrackDetails trackId={trackId}/>
         </aside>
     )
