90a8f80e539e3bb1b0e54b3a8d86cbc3d4c8c444
ashuksu <ashuksu@gmail.com>
2024-05-05 09:56:01 +0200

    dev - add + replaced Game component and update SidebarMenu to include additional Title for Game section

---
+++ src/components/game/Counter.tsx
@@ -0,0 +1,33 @@
+type Props = {
+    count: number
+    setCount: (count: number) => void
+    onFinish?: () => void
+}
+
+export function Counter(props: Props) {
+
+    const endCount = 5
+
+    const handleClick = () => {
+        let curentCount = props.count + 1;
+        props.setCount(curentCount)
+
+        if (curentCount === endCount && props.onFinish) {
+            props.onFinish()
+        }
+    }
+
+    return (
+        <>
+            <h2 className='py-3'>Нажми на кнопку {endCount} раза, чтобы увидеть слона</h2>
+            <button
+                className={`flex items-center justify-center w-max gap-2 py-2.5 px-4
+                    bg-amber-500 rounded-md text-black  cursor-pointer`}
+                onClick={handleClick}>
+                Counter:
+                <span className='font-bold'>{props.count}</span>
+            </button>
+        </>
+
+    )
+}
