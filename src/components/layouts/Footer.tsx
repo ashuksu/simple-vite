d0cdac600bbdb73b01019572b018ba9a72e84c74
ashuksu <ashuksu@gmail.com>
2024-05-18 12:17:06 +0200

    dev - refactored project, structure, naming

--- src/components/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='header shrink-0 bg-gray-800'>
-            <div className="container flex justify-center items-center py-3  text-white">
-                Footer
-            </div>
-        </footer>
-    )
-}
--- src/components/layouts/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <header className='header shrink-0 bg-gray-800'>
-            <div className="container flex justify-center items-center py-3  text-white">
-                Header
-            </div>
-        </header>
-    )
-}
