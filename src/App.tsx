303d64d796bb0cdbf7bf018225711e2ef97b8aa6
ashuksu <ashuksu@gmail.com>
2024-05-18 13:20:13 +0200

    custom-hook - add Counter and Age components with state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import {useState} from "react";
+
 export function App() {
   return (
-    <>
-        Hello world!
-    </>
+    <main className="p-5 flex flex-col gap-5">
+        <Counter/>
+        <Age/>
+    </main>
   )
 }
 
+export function Counter() {
+    const [value, setValue] = useState(1);
+
+    return (
+        <button
+            className='border-amber-50 border-2 p-5 w-50'
+            onClick={() => setValue(value + 1)}>Counter: {value}</button>
+    );
+}
+
+export function Age() {
+    const [value, setValue] = useState(10);
+
+    return (
+        <button
+            className='border-amber-50 border-2 p-5 w-50'
+            onClick={() => setValue(value + 1)}>Age: {value}</button>
+    );
+}
