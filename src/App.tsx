26a3a62ce263cb7a5ece83d76ce750549395caed
ashuksu <ashuksu@gmail.com>
2024-05-19 09:04:18 +0200

    light-switch - add NotificationSwitch and VisibilityToggle components; update LightSwitch2 with reset functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import {useState} from "react";
 
 export function App() {
     return (
-        <>
+        <div className="flex flex-col gap-3 items-start p-5">
             <LightSwitch/>
-            <br/>
             <LightSwitch2/>
-        </>
+            <VisibilityToggle/>
+            <NotificationSwitch/>
+            <br/>
+            <TextPage/>
+        </div>
     )
 }
 
@@ -17,7 +20,30 @@ function useToggle(initialValue: boolean = false) {
         setIsOn(value => !value)
     }
 
-    return {isOn, toggle, setIsOn}
+    const reset = () => {
+        setIsOn(initialValue)
+    }
+
+    return {isOn, toggle, setIsOn, reset}
+}
+
+function NotificationSwitch() {
+    const {isOn, toggle, setIsOn, reset} = useToggle(true)
+
+    return (
+        <div className='flex flex-col gap-3'>
+            {isOn ? <h2>🔔 Уведомления включены</h2> : <h2>🔕 Уведомления выключены</h2>}
+
+            <div className='flex gap-3'>
+                <button onClick={toggle}>Переключить (сообщения)</button>
+                {/*переключает состояние*/}
+                <button onClick={() => setIsOn(true)}>Включить Сообщения</button>
+                {/*устанавливает true*/}
+                <button onClick={reset}>Сбросить (сообщения) по умолчанию</button>
+                {/*возвращает к начальному состоянию*/}
+            </div>
+        </div>
+    )
 }
 
 export const LightSwitch = () => {
@@ -32,15 +58,95 @@ export const LightSwitch = () => {
 }
 
 export const LightSwitch2 = () => {
-    const {isOn, toggle, setIsOn} = useToggle(true)
+    const {isOn, toggle, setIsOn, reset} = useToggle(false)
+
+    return (
+        <>
+            <h2>{isOn ? "💡 Свет включен" : "🌙 Свет выключен"}</h2>
+            <button onClick={toggle}>Переключить свет</button>
+            <div className='flex gap-3'>
+                <button onClick={() => setIsOn(true)}>Включить 💡</button>
+                <button onClick={() => setIsOn(false)}>Выключить 🌙</button>
+            </div>
+            <button onClick={() => reset()}>Сбросить</button>
+        </>
+    )
+}
+
+function VisibilityToggle() {
+    const {isOn, setIsOn} = useToggle(false)
 
     return (
         <div>
-            <h2>{isOn ? "💡 Свет включен" : "🌙 Свет выключен"}</h2>
-            <button onClick={toggle}>Переключить свет</button>
+            <div className='flex gap-3'>
+                <button onClick={() => {
+                    setIsOn(true)
+                }}>
+                    Показать
+                </button>
+                <button onClick={() => {
+                    setIsOn(false)
+                }}>
+                    Скрыть
+                </button>
+
+            </div>
             <br/>
-            <button onClick={() => setIsOn(true)}>Включить 💡</button>
-            <button onClick={() => setIsOn(false)}>Выключить 🌙</button>
+            {isOn && (
+                <div>
+                    🎉 Это секретное сообщение!
+                </div>
+            )}
         </div>
+    )
+}
+
+
+function useText(initialText: string = '') {
+    const [text, setText] = useState<string>(initialText)
+    const clear = () => setText('...')
+    const toUpperCase = () => setText(text.toUpperCase());
+    const toLowerCase = () => setText(text.toLowerCase())
+
+    return {text, setText, clear, toUpperCase, toLowerCase}
+}
+
+function TitleEditor() {
+    const {text, toUpperCase, toLowerCase, clear} = useText("Заголовок статьи")
+
+    return (
+        <div className='flex flex-col gap-3'>
+            <h2>{text}</h2>
+            <button onClick={toUpperCase}>ВЕРХНИЙ РЕГИСТР</button>
+            <button onClick={toLowerCase}>нижний регистр</button>
+            <button onClick={clear}>Очистить</button>
+        </div>
+
+    )
+}
+
+function GreetingCard() {
+    const {text, setText, toUpperCase, toLowerCase, clear} = useText("Привет!")
+
+    return (
+        <div className='flex flex-col gap-3'>
+            💬 {text}
+            <button onClick={toUpperCase}>ГРОМКО</button>
+            <button onClick={toLowerCase}>тихо</button>
+            <button onClick={() => setText('Добро пожаловать!')}>Сказать 'Добро пожаловать!'</button>
+            <button onClick={clear}>Молчать</button>
+        </div>
+    )
+}
+
+function TextPage() {
+    return (
+        <>
+            <TitleEditor/>
+            <br/>
+            <br/>
+            <hr/>
+            <GreetingCard/>
+        </>
     )
 }
