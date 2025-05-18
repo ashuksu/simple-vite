8263e8d01c9c8319da604555591e6a381b88097c
Ashuksu <ashuksu@gmail.com>
2025-05-18 07:04:25 +0200

    main - initialize project, add package.json

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Simple project using Vite for the frontend and PHP for the backend.">
+    <title>PHP + Vite Template</title>
+</head>
+<body>
+<div>Hello world!</div>
+</body>
+</html>
