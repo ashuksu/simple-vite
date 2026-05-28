36ff0ab41ed3b8bdf4d0b0598bf9cd8279f16912
ashuksu <ashuksu@gmail.com>
2026-05-28 18:25:24 +0200

    dev - fetch user data from API and display loading state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import './App.css'
+import {useEffect, useState} from "react";
+
+interface User {
+    id: number;
+    name: string;
+}
 
 function App() {
-  return (
-    <>
-    </>
-  )
+    const [data, setData] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((json: User[]) => {
+                setData(json);
+                setLoading(false);
+            })
+            .catch((error: unknown) => {
+                console.error('Error: ', error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <main className="py-10 px-4">
+            <div
+                className="flex flex-col items-center justify-center max-w-100 bg-gray-200 rounded-lg p-6 text-gray-700 text-lg">
+                {loading ? (
+                    <div>Loading...</div>
+                ) : data.length === 0 ? (
+                    <div>No Users</div>
+                ) : (
+                    data.map((item) => (
+                        <div key={item.id}>{item.name}</div>
+                    ))
+                )}
+            </div>
+        </main>
+    )
 }
 
 export default App
