5038fea73359ea919b6458da9ca69c1eafc91825
ashuksu <ashuksu@gmail.com>
2026-05-29 18:54:32 +0200

    dev - update Title component imports and add margin to improve layout in Main and SidebarMenu

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -1,5 +1,5 @@
-import Title from "../ui/Title.tsx";
-import TrackList from "../TrackList.tsx";
+import Title from "../ui/Title";
+import TrackList from "../TrackList";
 
 interface MainProps {
     trackId: string | null;
@@ -9,7 +9,7 @@ interface MainProps {
 export default function Main({ trackId, onTrackSelect }: MainProps) {
     return (
         <section className="section w-1/2">
-            <Title title='Track List' styleCSS='text-gray-50'/>
+            <Title title='Track List' styleCSS='text-gray-50 mb-5'/>
             <TrackList selectedTrackId={trackId} onTrackSelect={onTrackSelect}/>
         </section>
     );
