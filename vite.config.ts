accbdd2eea256dc09edea0bb143058554c5f50d0
ashuksu <ashuksu@gmail.com>
2026-08-18 19:06:03 +0200

    fix: update base path in vite.config.ts for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'simple-vite',
+  base: '/simple-vite/',
   plugins: [
     react(),
     tailwindcss(),
