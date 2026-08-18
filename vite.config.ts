98e66117655d697dc89a3466718f7100fddbcea2
ashuksu <ashuksu@gmail.com>
2026-08-18 20:48:56 +0200

    fix: update base path in vite.config.ts to handle production and Vercel environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import pkg from './package.json' with {type: 'json'}
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/simple-vite/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.BASE_PATH || process.env.BASE_PATH || `/${pkg.name}/`,
+
+        plugins: [
+            react(),
+            tailwindcss(),
+        ],
+    }
 })
