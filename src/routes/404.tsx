a31af1b520ca153500f3bd36ed3ad9d98d9b5f5a
ashuksu <ashuksu@gmail.com>
2026-08-23 17:53:17 +0200

    tanstack-query - feat: add meta tags and head content for routing pages

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -2,5 +2,10 @@ import {createFileRoute} from '@tanstack/react-router'
 import {NotFoundPage} from "../ui/pages/NotFoundPage.tsx";
 
 export const Route = createFileRoute('/404')({
+    head: () => ({
+        meta: [{
+            title: 'Not Found Page',
+        }]
+    }),
     component: NotFoundPage,
 })
